83b3c056d15ea3b1c257dae6c348b8f27a6f70df
luggiestar <luggiestar@gmail.com>
2025-12-11 15:15:26 +0300

    payment

--- payment/signals.py
+++ payment/signals.py
@@ -1,10 +1,10 @@
+from datetime import timedelta
+
 from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
-from django.utils import timezone
 
 from payment.actions import generate_order_action
-from payment.models import CustomerOrder, CustomerOrderPayment
-from payment.selectors import get_group_fee, get_current_static_config
+from payment.models import CustomerOrderPayment
 from users.models import User
 from utils.logger import AppLogger
 
@@ -25,6 +25,7 @@ def update_customer_order_on_payment(sender, instance, created, **kwargs):
             if instance.payment_status == "COMPLETED" and instance.result == "SUCCESS":
                 logger.info(f"Payment completed for order {instance.order.order_id}")
                 instance.order.is_paid = True
+                instance.order.next_payment_date = instance.created_at + timedelta(days=30)
                 instance.order.save()
         else:
             logger.error(f"Payment saved successfully but order is not updated")
